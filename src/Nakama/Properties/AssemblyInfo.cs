ae6f2ee39ffe133e43b6ed03d12e8c0824be4831
Chris Molozian <chris@heroiclabs.com>
2018-08-17 04:24:21 +0100

    Update assembly version.

--- src/Nakama/Properties/AssemblyInfo.cs
+++ src/Nakama/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("2.0.0+43.Branch.master.Sha.a1badb5527c2b27a6dea099deb51a7aa6661c4ea")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.1.0+Branch.master.Sha.f44c52db42520cfabe32e36d5d44292442b88738")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
