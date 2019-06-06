ba6332b968f11db7457cd8efb2116d2588e4194b
Chris Molozian <chris@heroiclabs.com>
2019-06-06 09:58:08 +0100

    Replace build system with dotnet cli.

--- src/Nakama.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using NUnit.Framework;
-
-[assembly: Parallelizable(ParallelScope.Fixtures)]
-
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0+Branch.master.Sha.f44c52db42520cfabe32e36d5d44292442b88738")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
