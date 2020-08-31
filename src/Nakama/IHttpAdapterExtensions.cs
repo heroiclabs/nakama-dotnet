086f975a940e9cffab007ccfa358919a04bfec66
Chris Molozian <chris@heroiclabs.com>
2020-08-31 22:47:13 +0100

    Rename HTTP adapter extension class.

--- src/Nakama/IHttpAdapterExtensions.cs
+++ src/Nakama/IHttpAdapterExtensions.cs
@@ -1,4 +1,3 @@
-
 /**
  * Copyright 2020 The Nakama Authors
  *
@@ -22,15 +21,15 @@ namespace Nakama
     /// <summary>
     /// Extension methods for the <see cref="IHttpAdapter"> interface.
     /// </summary>
-    public static class IHttpAdapterExt
+    public static class IHttpAdapterExtensions
     {
         /// <summary>
         /// Performs an in-place copy of keys and values from Nakama's error response dictionary into
-        /// the data dictionary of an <see cref="ApiResponseException"/>.
+        /// the data dictionary of an <see cref="Nakama.ApiResponseException"/>.
+        /// </summary>
         /// <param name="adapter">The adapter receiving the error response.</param>
         /// <param name="decodedResponse"> The decoded error response from the server.</param>
         /// <param name="e"> The exception whose data dictionary is being written to.</param>
-        /// </summary>
         public static void CopyErrorDictionary(this IHttpAdapter adapter, Dictionary<string, object> decodedResponse, ApiResponseException e)
         {
             var errDict = decodedResponse["error"] as Dictionary<string, object>;
