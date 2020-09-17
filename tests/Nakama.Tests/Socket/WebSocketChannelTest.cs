f68989a31e9833c2271c1eec9a7d6a15be822886
Luke Gehorsam <luke@heroiclabs.com>
2020-09-17 12:50:16 -0400

    load client from settings file in web socket channel test

--- tests/Nakama.Tests/Socket/WebSocketChannelTest.cs
+++ tests/Nakama.Tests/Socket/WebSocketChannelTest.cs
@@ -29,7 +29,7 @@ namespace Nakama.Tests.Socket
 
         public WebSocketChannelTest()
         {
-            _client = new Client("http", "defaultkey", 7350, "127.0.0.1");
+            _client = ClientUtil.FromSettingsFile();
             _socket = Nakama.Socket.From(_client);
         }
 
