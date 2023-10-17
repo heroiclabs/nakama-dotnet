73ae53124313ea9310b96de08ab9b02d49d0faef
Luke Gehorsam <luke@heroiclabs.com>
2023-10-17 12:37:47 -0400

    Add more basic friends test

--- Nakama.Tests/FriendTest.cs
+++ Nakama.Tests/FriendTest.cs
@@ -53,5 +53,45 @@ namespace Nakama.Tests.Api
             Assert.Equal(3, friendList.Friends.First().State); // banned
             Assert.Null(session2Notif);
         }
+
+        [Fact(Timeout = TestsUtil.TIMEOUT_MILLISECONDS)]
+        public async Task FriendsShouldBeAddedAndAccepted()
+        {
+            var session = await _client.AuthenticateCustomAsync($"{Guid.NewGuid()}");
+            var session2 = await _client.AuthenticateCustomAsync($"{Guid.NewGuid()}");
+
+            var socket2 = Nakama.Socket.From(_client);
+
+            await _socket.ConnectAsync(session);
+            await socket2.ConnectAsync(session2);
+
+            IApiNotification? session1Notif = null;
+
+            _socket.ReceivedNotification += (IApiNotification notif) => {
+                session1Notif = notif;
+            };
+
+            IApiNotification? session2Notif = null;
+
+            socket2.ReceivedNotification += (IApiNotification notif) => {
+                session2Notif = notif;
+            };
+
+            await _client.AddFriendsAsync(session, new string[]{session2.UserId});
+
+            await Task.Delay(1000);
+            Assert.NotNull(session2Notif);
+
+            var friendList = await _client.ListFriendsAsync(session, 1); // has sent invitation
+            Assert.Single(friendList.Friends);
+
+            await _client.AddFriendsAsync(session2, new string[]{session.UserId});
+            await Task.Delay(1000);
+            Assert.NotNull(session1Notif);
+
+            friendList = await _client.ListFriendsAsync(session, 0); // friends
+
+            Assert.Single(friendList.Friends);
+        }
     }
 }
